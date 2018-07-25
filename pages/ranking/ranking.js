450ba8f812a26c7da95a90dcba73173b23011337
coderyangl <798957387@qq.com>
2018-07-25 15:40:33 +0800

    Initial commit

---
+++ pages/ranking/ranking.js
@@ -0,0 +1,79 @@
+// pages/ranking/ranking.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      female:[],
+      male:[]
+  },
+    getRankData: function(){
+        wx.request({
+            url: 'http://api.zhuishushenqi.com/ranking/gender',
+            success:(res)=>{
+                console.log(res.data);
+                this.setData({
+                    female: res.data.female,
+                    male:res.data.male
+                });
+            }
+        })
+    },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+     this. getRankData();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
